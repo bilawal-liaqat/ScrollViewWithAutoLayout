bf90743925d4e64de6a7788c41bb28097de8533f
Bilawal Liaqat <bilawalliaqat@NX00313-Mac-mini.local>
2016-05-16 12:03:13 +0500

    storyboard update

--- layoutScroll/ViewController.swift
+++ layoutScroll/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var srollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        srollView.contentSize = contentView.frame.size;
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
